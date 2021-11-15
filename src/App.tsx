d4a222ae48b034b1decd2a0d95e58fa46c582dff
Kamil Put <kamil.put@interia.pl>
2021-11-15 09:17:55 +0100

    Add data and state to 'App' component with hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const App = () => {
+interface Book {
+  id: string;
+  title: string;
+  author: string;
+  price: number;
+}
+
+const booksData: Book[] = [
+  { id: 'te1314derw', title: 'Of Mice and Men', author: 'John Steinbeck', price: 25 },
+  { id: 't32s14dzdw', title: 'East of Eden', author: 'John Steinbeck', price: 30 }
+];
+
+const App: React.FC = () => {
+  const [books, setBooks] = useState(booksData);
+  
   return (
     <div className="container">
       <header>
          <h1>Books App</h1>
       </header>
+      {books.map((book: Book) => <span>{book.title}</span>)}
     </div>
   );
 }
